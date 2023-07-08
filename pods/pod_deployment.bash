f79b83047d04f88132265f72396d79540e0aef08
Carlos Rodrigo Rivero <85003009+rodrorivero@users.noreply.github.com>
2023-07-08 13:59:24 -0400

    Create pod_deployment.bash

---
+++ pods/pod_deployment.bash
@@ -0,0 +1,12 @@
+# Niginx pod deployment
+# Creating nginx pod
+kubectl run nginx --image nginx
+# List all pods in a default namespace
+kubectl  get pods
+# List all pods in all namespaces
+kubectl  get pods --all-namespaces
+# Retrieve pod's descriptor
+kubectl  get pod nginx -o yaml 
+# Access nginx web server running in a pod
+kubectl port-forward nginx 8080:80
+curl localhost:8080 # Send this command in a new terminal session
